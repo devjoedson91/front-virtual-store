aba8b746bd2d7350cee102660a4ab05f968492d1
Joedson Ferreira <joedson.devel@gmail.com>
2022-09-29 16:04:40 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import '../../styles/main.css';
 import '../../styles/globals.scss';
-import NavBar from '../components/NavBar';
+import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { CartProvider } from '../hooks/useCart';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
-      <CartProvider>
-        
+      <CartProvider>  
         <Component {...pageProps} />
         <ToastContainer autoClose={3000} />
       </CartProvider>
